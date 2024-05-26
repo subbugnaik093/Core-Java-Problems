bbbcbdfa182c400e62ec2171d840cdda2760e6b4
Subrahmanya Naik <subbugnaik093@gmail.com>
2024-05-26 13:46:53 +0530

    While Loop

---
+++ Whileloop.java
@@ -0,0 +1,18 @@
+public class Whileloop {
+    public static void main(String[] args) {
+        int i =1;
+        while (i<=5){
+           
+            System.out.println("Hello");
+            i++;
+            int j=1;
+            while(j<=i-1){
+                System.out.println("AM doing");
+                j++;
+            
+            }
+        
+        }
+    }
+    
+}
