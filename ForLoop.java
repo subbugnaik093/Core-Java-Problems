86fddea7b659bf4d0f731b4ee6f8dec96833fb9b
Subrahmanya Naik <subbugnaik093@gmail.com>
2024-05-26 14:28:43 +0530

    For Loop

---
+++ ForLoop.java
@@ -0,0 +1,14 @@
+/**
+ * ForLoop
+ */
+public class ForLoop {
+
+    public static void main(String[] args) {
+        for(int i=1; i<=5; i++){
+            System.out.println("Day " + i);
+            for(int j=1; j<=9;j++){
+                System.out.println(" "+ (j+8)+" - "+ (j+9));
+            }
+        }
+    }
+}
