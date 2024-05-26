2ef55c44597a37eb4eb52f2ad166a129ba6f8b2b
Subrahmanya Naik <subbugnaik093@gmail.com>
2024-05-26 11:58:09 +0530

    Swith statement

---
+++ Hello.java
@@ -0,0 +1,33 @@
+class Hello{
+public static void main(String []args){
+  // Switch statement
+   int day =9;
+  switch (day) {
+      case 1:
+      System.out.println("Monday");
+      break;
+      case 2:
+      System.out.println("Tuesday");
+      break;
+      case 3:
+      System.out.println("Wensday");
+      break;
+      case 4:
+      System.out.println("Thursday");
+      break;
+      case 5:
+      System.out.println("Friday");
+      break;
+      case 6:
+      System.out.println("Saturday");
+      break;
+      case 7:
+      System.out.println("Sunday");
+      break;
+    default:
+    System.out.println("please enter proper day");
+      break;
+  }
+   
+}
+}
